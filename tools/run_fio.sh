711e98671c5459c19ec909dbe41eb812643ed7d0
Xueyang Liu <marliu@umich.edu>
2023-01-18 10:21:49 -0500

    Update tests

--- tools/run_fio.sh
+++ tools/run_fio.sh
@@ -1,10 +1,12 @@
 # Run all fio tests in one given directory
 dir=$1
+result_dir=$2 # result directory name
 cd $dir
-mkdir results
+# mkdir $result_dir
 # get file names from $dir
 for file in $(find . -name "*.fio"); do 
     # run fio
-    sudo fio $file > results/$file-results
-    echo "Saved test results to "results/$file-results
+    fio $file > $result_dir/$file-results
+    # sudo fio $file > $result_dir/$file-results
+    echo "Saved test results to "$result_dir/$file-results
 done
